83775b712a32a6d1775851813df0b8e342507143
福尔魔猪 <107930642+MagicPigZHN@users.noreply.github.com>
2023-06-20 14:09:46 +0800

    Update ExHentai.user.js
    更改图片路径

--- ExHentai.user.js
+++ ExHentai.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           Exhentai公共账号
 // @description    替换Exhentai的cookie，自动登录
-// @version        1.4.6
+// @version        1.4.7
 // @author         福尔魔猪
 // @icon           https://exhentai.org/favicon.ico
 // @match          *://*.exhentai.org/*
@@ -14,62 +14,62 @@
 // ==/UserScript==
 (function(){
     'use strict';
-    var expireDate = new Date();
+    var expireDate = 新建 日期();
     expireDate.setFullYear(expireDate.getFullYear() + 100);
 
-    if (location.host == 'exhentai.org' && document.cookie.split(';').length < 3) {
+    if (location.host == 'exhentai.org' && document.cookie。分屏(';')。length < 3) {
         //背景
         var img = document.createElement("img");
-        img.src = "https://c4.kemono.party/data/b3/78/b378113d5d01c9f15aa40622d30035a784613c8d16d80d023e37fb477747d310.jpg?f=001560d6-17f0-4df8-98e7-244d8601dfbd.jpg";
-        img.style.position = "fixed";
-        img.style.top = "0";
-        img.style.left = "0";
-        img.style.width = "100%";
-        img.style.height = "100%";
-        img.style.objectFit = "cover";
-        document.body.appendChild(img);
+        img.src = "https://github.com/MagicPigZHN/Exhentai-Shared-Account/blob/master/picture/background.jpg?raw=true";
+        img.style。position = "fixed";
+        img.style。top = "0";
+        img.style。left = "0";
+        img.style。width = "100%";
+        img.style。height = "100%";
+        img.style。objectFit = "cover";
+        document.内容。appendChild(img);
 
         //UI窗口
         var dialog = document.createElement("div");
-        dialog.style.position = "fixed";
-        dialog.style.top = "50%";
-        dialog.style.left = "50%";
-        dialog.style.transform = "translate(-50%, -50%)";
-        dialog.style.background = "rgba(255, 192, 203, 0.5)";
-        dialog.style.padding = "20px";
-        dialog.style.borderRadius = "5px";
-        dialog.style.boxShadow = "0px 0px 10px 1px rgba(0, 0, 0, 0.5)";
+        dialog.style。position = "fixed";
+        dialog.style。top = "50%";
+        dialog.style。left = "50%";
+        dialog.style。transform = "translate(-50%, -50%)";
+        dialog.style。background = "rgba(255, 192, 203, 0.5)";
+        dialog.style。padding = "20px";
+        dialog.style。borderRadius = "5px";
+        dialog.style。boxShadow = "0px 0px 10px 1px rgba(0, 0, 0, 0.5)";
         dialog.innerHTML = "<p style='font-size: 35px; text-align: center; font-family: \"仿宋\", sans-serif; font-weight: bold; color: #9370DB;'>请选择公共账号：</p>";
 
         //按钮1
         var button1 = document.createElement("button");
-        button1.style.backgroundColor = "#ffd4fd";
-        button1.style.margin = "10px";
-        button1.style.padding = "5px 10px";
-        button1.style.borderRadius = "5px";
-        button1.style.border = "none";
-        button1.style.cursor = "pointer";
-        button1.style.fontFamily = "仿宋";
-        button1.style.color = "#d058ea";
-        button1.style.fontWeight = "bold";
-        button1.style.fontSize = "20px";
-        button1.style.boxShadow = "0px 2px 4px rgba(0, 0, 0, 0.2)";
-        button1.style.backgroundImage = "linear-gradient(to bottom, #ffd4fd, #ffb2e8)";
+        button1.style。backgroundColor = "#ffd4fd";
+        button1.style。margin = "10px";
+        button1.style。padding = "5px 10px";
+        button1.style。borderRadius = "5px";
+        button1.style。border = "none";
+        button1.style。cursor = "pointer";
+        button1.style。fontFamily = "仿宋";
+        button1.style。color = "#d058ea";
+        button1.style。fontWeight = "bold";
+        button1.style。fontSize = "20px";
+        button1.style。boxShadow = "0px 2px 4px rgba(0, 0, 0, 0.2)";
+        button1.style。backgroundImage = "linear-gradient(to bottom, #ffd4fd, #ffb2e8)";
         button1.innerHTML = "账号1";
 
         button1.onmousedown = function() {
-            button1.style.boxShadow = "none";
-            button1.style.transform = "translateY(2px)";
+            button1.style。boxShadow = "none";
+            button1.style。transform = "translateY(2px)";
         };
 
         button1.onmouseup = function() {
-            button1.style.boxShadow = "0px 2px 4px rgba(0, 0, 0, 0.2)";
-            button1.style.transform = "none";
+            button1.style。boxShadow = "0px 2px 4px rgba(0, 0, 0, 0.2)";
+            button1.style。transform = "none";
         };
 
         button1.onmouseleave = function() {
-            button1.style.boxShadow = "0px 2px 4px rgba(0, 0, 0, 0.2)";
-            button1.style.transform = "none";
+            button1.style。boxShadow = "0px 2px 4px rgba(0, 0, 0, 0.2)";
+            button1.style。transform = "none";
         };
 
         button1.onclick = function() {
@@ -80,33 +80,33 @@
 
         //按钮2
         var button2 = document.createElement("button");
-        button2.style.backgroundColor = "#ffd4fd";
-        button2.style.margin = "10px";
-        button2.style.padding = "5px 10px";
-        button2.style.borderRadius = "5px";
-        button2.style.border = "none";
-        button2.style.cursor = "pointer";
-        button2.style.fontFamily = "仿宋";
-        button2.style.fontWeight = "bold";
-        button2.style.color = "#d058ea";
-        button2.style.fontSize = "20px";
-        button2.style.boxShadow = "0px 2px 4px rgba(0, 0, 0, 0.2)";
-        button2.style.backgroundImage = "linear-gradient(to bottom, #ffd4fd, #ffb2e8)";
+        button2.style。backgroundColor = "#ffd4fd";
+        button2.style。margin = "10px";
+        button2.style。padding = "5px 10px";
+        button2.style。borderRadius = "5px";
+        button2.style。border = "none";
+        button2.style。cursor = "pointer";
+        button2.style。fontFamily = "仿宋";
+        button2.style。fontWeight = "bold";
+        button2.style。color = "#d058ea";
+        button2.style。fontSize = "20px";
+        button2.style。boxShadow = "0px 2px 4px rgba(0, 0, 0, 0.2)";
+        button2.style。backgroundImage = "linear-gradient(to bottom, #ffd4fd, #ffb2e8)";
         button2.innerHTML = "账号2";
 
         button2.onmousedown = function() {
-            button2.style.boxShadow = "none";
-            button2.style.transform = "translateY(2px)";
+            button2.style。boxShadow = "none";
+            button2.style。transform = "translateY(2px)";
         };
 
         button2.onmouseup = function() {
-            button2.style.boxShadow = "0px 2px 4px rgba(0, 0, 0, 0.2)";
-            button2.style.transform = "none";
+            button2.style。boxShadow = "0px 2px 4px rgba(0, 0, 0, 0.2)";
+            button2.style。transform = "none";
         };
 
         button2.onmouseleave = function() {
-            button2.style.boxShadow = "0px 2px 4px rgba(0, 0, 0, 0.2)";
-            button2.style.transform = "none";
+            button2.style。boxShadow = "0px 2px 4px rgba(0, 0, 0, 0.2)";
+            button2.style。transform = "none";
         };
 
         button2.onclick = function() {
@@ -181,7 +181,7 @@
     } else if (location.host == 'exhentai.org' && document.cookie。分屏(';')。length >= 3){
         // 点击图标更换账号
         var icon = document.createElement('img');
-        icon.src = 'https://kemono.party/icons/fanbox/50258193';
+        icon.src = 'https://github.com/MagicPigZHN/Exhentai-Shared-Account/blob/master/picture/icon.jpg?raw=true';
         icon.style。position = 'fixed';
         icon.style。bottom = '50px';
         icon.style。right = '5px';
